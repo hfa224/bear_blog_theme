4a318a520045b1be442ff1313a7d4b7a2469f2e7
hfa224 <helenffionadams@gmail.com>
2024-08-25 20:46:38 +0100

    update image_resize.py, tidy up, better logging and commenting

--- image_resize.py
+++ image_resize.py
@@ -5,60 +5,45 @@ import re
 import sys
 import math
 
-print(sys.argv)
-
-def linkrepl(matchobj):
+def image_title_repl(matchobj):
     """replace img_title with img_title_resized"""
     file_name = matchobj.group(0)
     return file_name + "_resized"
 
 percentage = int(sys.argv[1]) if len(sys.argv) > 1 else 50
 
-print(percentage)
+print("Resizing to " + str(percentage) + "%")
 
 if not isinstance(percentage, (int, float, complex)):
     raise TypeError("Argument must be a percentage number, argument was " + percentage)
 
-# resize dimensions
-resize_dimensions = {
-    'phone': {
-        'v': (907, 1209),
-        'h': (1613, 1210)
-    },
-    'film': {
-        'v': (),
-        'h':(1252, 830)
-    }
-}
-film_photo_h = (1252, 830)
-phone_photo_v = (907, 1209)
-film_photo_v = ()
-phone_photo_h = (1613, 1210)
-
 # list all images in the folder
 current_dir = getcwd()
-print(current_dir)
-for f in listdir(current_dir):
-    print(f)
-onlyimagefiles = [f for f in listdir(current_dir) if isfile(join(current_dir, f)) and bool(re.search(r"\.(gif|jpe?g|tiff?|png|webp|bmp)", f))]
+print("The current directory is " + current_dir)
+onlyimagefiles = [
+    f
+    for f in listdir(current_dir)
+    if isfile(join(current_dir, f))
+    and bool(re.search(r"\.(gif|jpe?g|tiff?|png|webp|bmp)", f))
+]
 
-print(onlyimagefiles)
+# list files in image
+print("Found the following image files: ", onlyimagefiles)
 
 for image_file in onlyimagefiles:
     image = Image.open(image_file)
 
-    new_height = math.floor(image.height * (percentage/100))
-    new_width = math.floor(image.width * (percentage/100))
+    new_height = math.floor(image.height * (percentage / 100))
+    new_width = math.floor(image.width * (percentage / 100))
 
     new_image = image.resize((new_width, new_height))
 
     link_regex = re.compile(
         r"[^\.]+"
-    )  # match anything in the description in format [words words]
-    new_filename = link_regex.sub(linkrepl, image_file, 1)
+    ) 
+    new_filename = link_regex.sub(image_title_repl, image_file, 1)
+
+    # save under the new filename
     print(new_filename)
     new_image.save(new_filename)
 
-# loop through and resize them all
-
-# two different resizings - one for horizontal, one for vertical
